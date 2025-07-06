c79695856d542534b135d7f65675489ed55dd2df
Magnteo <bogdanserg48@gmail.com>
2025-07-06 19:51:28 +0300

    yes

---
+++ 2/2project.py
@@ -0,0 +1,17 @@
+def get_cats_info(path):
+    try:
+        cats=[]
+        with open(path,'r', encoding="utf-8") as file:
+            for  cat in file :
+                try:
+                    cat_id, name, age =cat.strip().split(",") 
+                    cat_dict ={"id": cat_id, "name": name, "age": age}
+                    cats.append(cat_dict)
+                except ValueError:
+                    print("рядок не має 3 частин")
+                    continue
+        return cats
+    except FileNotFoundError:
+        return "невірний шлях"
+cats_info = get_cats_info("cat.txt")
+print(cats_info)
