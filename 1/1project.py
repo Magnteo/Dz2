adeaaa6022c9e77b6c59acaab3ef298b3d1c82d5
Magnteo <bogdanserg48@gmail.com>
2025-07-06 12:51:18 +0300

    б

---
+++ 1/1project.py
@@ -0,0 +1,22 @@
+def total_salary(path):
+    try:
+        total = 0
+        count =0
+        with open(path, "r", encoding="utf-8") as file:
+                for line in file:
+                    try:
+                        name ,salary = line.strip().split(",")
+                        count +=1
+                        salary = int(salary)
+                        total = total+salary
+                    except ValueError:
+                         print("Помилка")
+        
+        average = total / count
+        return total,average
+    except FileNotFoundError:
+        return f"Файл за шляхом '{path}' не знайдено!"
+        
+total, average = total_salary("salaries.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+#я не розумію чосу тут необроблються помилки я вже чата питав а він якусь дуристику пише  
